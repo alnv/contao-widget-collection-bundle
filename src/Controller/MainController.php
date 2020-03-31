2d7370179cdfacd12635e9a63785e5e16a3d1456
Alexander Naumov <mail@alexandernaumov.de>
2020-03-31 16:56:10 +0200

    improve controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,17 @@
 
 namespace Alnv\ContaoWidgetCollectionBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Alnv\ContaoWidgetCollectionBundle\Helpers\Toolkit;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  *
  * @Route("/widget-collection", defaults={"_token_check" = false})
  */
 class MainController extends Controller {
-
 
     /**
      *
@@ -22,26 +21,19 @@ class MainController extends Controller {
      */
     public function getAjaxSelectOptions() {
 
-        $arrReturn = [
-            'options' => []
-        ];
+        $arrReturn = ['options' => []];
         $this->container->get( 'contao.framework' )->initialize();
-
         $objDatabase = \Database::getInstance();
         $objField = $objDatabase->prepare('SELECT * FROM tl_form_field WHERE id = ?')->limit( 1 )->execute( \Input::get('id') );
 
         if ( $objField->numRows ) {
-
             $strTable = $objField->table;
             $strValueColumn = $objField->vColumn;
             $strLabelColumn = $objField->lColumn;
-
             $objEntities= $objDatabase->prepare('SELECT * FROM '. $strTable . ' WHERE `'. $strLabelColumn .'` REGEXP ? OR `'. $strValueColumn .'` REGEXP ?' )->execute( \Input::get('search'), \Input::get('search') );
 
             if ( $objEntities->numRows ) {
-
                 while ( $objEntities->next() ) {
-
                     $arrReturn['options'][] = [
                         'label' => $objEntities->{$strLabelColumn},
                         'value' => $objEntities->{$strValueColumn}
@@ -49,24 +41,18 @@ class MainController extends Controller {
                 }
             }
         }
-
-        header('Content-Type: application/json');
-        echo json_encode( $arrReturn, 512 );
-        exit;
+        return new JsonResponse($arrReturn);
     }
-
 
     /**
      *
      * @Route("/combo-wizard", name="fetchComboWizard")
      * @Method({"POST"})
      */
-    public function fetchComboWizard() {
+    public function fetchComboWizard(Request $objRequest) {
 
-        $this->container->get( 'contao.framework' )->initialize();
-        \System::loadLanguageFile( 'default', \Input::post('language') );
-        header('Content-Type: application/json');
-        echo json_encode( Toolkit::getEvalByTableAndFieldname( \Input::post('table'), \Input::post('name') ), 512 );
-        exit;
+        $this->container->get('contao.framework')->initialize();
+        \System::loadLanguageFile('default', $objRequest->request->get('language'));
+        return new JsonResponse(\Alnv\ContaoWidgetCollectionBundle\Helpers\Toolkit::getEvalByTableAndFieldname($objRequest->request->get('table'),$objRequest->request->get('name'),$objRequest->request->get('id')));
     }
 }
--- src/Helpers/Toolkit.php
+++ src/Helpers/Toolkit.php
@@ -2,83 +2,64 @@
 
 namespace Alnv\ContaoWidgetCollectionBundle\Helpers;
 
-
 class Toolkit {
 
+    public static function getEvalByTableAndFieldname($strTable, $strFieldname, $strId=null) {
 
-    public static function getEvalByTableAndFieldname( $strTable, $strFieldname ) {
-
-        \Controller::loadDataContainer( $strTable );
-        \System::loadLanguageFile( $strTable );
-
+        \Controller::loadDataContainer($strTable);
+        \System::loadLanguageFile($strTable);
         $objVDataContainer = new VDataContainer();
-        $objVDataContainer->activeRecord = static::getActiveRecordFromRouting();
-
-        $arrField = \Widget::getAttributesFromDca( $GLOBALS['TL_DCA'][ $strTable ]['fields'][ $strFieldname ], $strFieldname, '', $strFieldname, $strTable, $objVDataContainer );
-
+        $objVDataContainer->activeRecord = static::getActiveRecordFromRouting($strTable,$strId);
+        $arrField = \Widget::getAttributesFromDca($GLOBALS['TL_DCA'][$strTable]['fields'][ $strFieldname ], $strFieldname, '', $strFieldname, $strTable, $objVDataContainer);
         return $arrField;
     }
 
-
-    protected function getActiveRecordFromRouting() {
+    protected static function getActiveRecordFromRouting($strTable=null,$strId=null) {
 
         $objActiveRecord = new \stdClass();
-        $strTable = \Input::post('table');
-        $strId = \Input::post('id');
+        $strTable = $strTable ?: \Input::post('table');
+        $strId = $strId ?: \Input::post('id');
 
-        if ( !$strTable || !$strId ) {
-
+        if (!$strTable || !$strId) {
             return $objActiveRecord;
         }
 
-        $objDatabase = \Database::getInstance();
-        $objEntity = $objDatabase->prepare('SELECT * FROM ' . $strTable . ' WHERE id = ?' )->limit(1)->execute( $strId );
+        $objEntity = \Database::getInstance()->prepare('SELECT * FROM ' . $strTable . ' WHERE id = ?' )->limit(1)->execute($strId);
 
         if ( $objEntity->numRows ) {
-
             $objActiveRecord = $objEntity;
         }
 
         return $objActiveRecord;
     }
 
-
     public static function decodeJson( $strJson, $arrMap = [] ) {
 
         if ( !$strJson ) {
-
             return [];
         }
 
         $arrJson = json_decode( $strJson, true );
 
         if ( !is_array( $arrJson ) || empty( $arrJson ) ) {
-
             return [];
         }
 
         $arrReturn = [];
 
         foreach ( $arrJson as $arrOptions ) {
-
             $arrOption = [];
-
             foreach ( $arrOptions as $strKey => $strValue ) {
-
                 if ( $strValue === null ) {
-
                     continue;
                 }
-
                 $strKey = $arrMap[ $strKey ] ?: $strKey;
                 $arrOption[ $strKey ] = (string) $strValue;
             }
 
             if ( empty( $arrOption ) ) {
-
                 continue;
             }
-
             $arrReturn[] = $arrOption;
         }
 
